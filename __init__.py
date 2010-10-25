0abf8f0cc025082026d4be06fa50f56a30d43465
graeme.mcferren <graeme.mcferren@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2010-10-25 20:49:56 +0000

    initial __init__.py file to pattern match against for others
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@3 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

---
+++ __init__.py
@@ -0,0 +1,35 @@
+###########################################################################
+##
+## Copyright (C) 2010 CSIR Meraka Institute. All rights reserved.
+##
+## This full package extends VisTrails, providing GIS/Earth Observation 
+## ingestion, pre-processing, transformation, analytic and visualisation 
+## capabilities . Included is the abilty to run code transparently in 
+## OpenNebula cloud environments. There are various software 
+## dependencies, but all are FOSS.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following to ensure GNU General Public
+## Licensing requirements will be met:
+## http://www.opensource.org/licenses/gpl-license.php
+##
+## If you are unsure which license is appropriate for your use (for
+## instance, you are interested in developing a commercial derivative
+## of VisTrails), please contact us at vistrails@sci.utah.edu.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+############################################################################
+"""This full package extends VisTrails, providing GIS/Earth Observation 
+ingestion, pre-processing, transformation, analytic and visualisation 
+capabilities . Included is the abilty to run code transparently in 
+OpenNebula cloud environments.
+"""
+
+identifier = 'za.co.csir.eo4vistrails'
+name = 'eo4vistrails'
+version = '0.0.1'
+
