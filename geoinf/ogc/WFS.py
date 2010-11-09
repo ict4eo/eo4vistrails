0f5e863872efefb640049bd2844b891f582850cd
bcwele@gmail.com <bcwele@gmail.com@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2010-11-09 11:59:29 +0000

    WFS buttons to pre-populate parameters, make necessary changes i.e. cordinates of choice then save
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@65 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

--- geoinf/ogc/WFS.py
+++ geoinf/ogc/WFS.py
@@ -64,69 +64,95 @@ class WFS2(QtGui.QWidget):
 		lineEdit_MinY = QtGui.QLineEdit("", self)
 		lineEdit_MinY.setGeometry(QtCore.QRect(270, 290, 80, 21))
 		lineEdit_MinY.setObjectName("lineEdit_MinY")
+		
 		lbl_MaxY = QtGui.QLabel("Max Y:", self)
 		lbl_MaxY.setGeometry(QtCore.QRect(220, 350, 50, 21))
-		lbl_MaxY.setObjectName("lbl_MaxY")
+		lbl_MaxY.setObjectName("lbl_MaxY")		
+		
 		lineEdit_MaxY = QtGui.QLineEdit("", self)
 		lineEdit_MaxY.setGeometry(QtCore.QRect(270, 350, 80, 21))
 		lineEdit_MaxY.setObjectName("lineEdit_MaxY")
+		
 		lblMinY = QtGui.QLabel("Min Y:", self)
-		lblMinY.setGeometry(QtCore.QRect(220, 290, 40, 20))
-		
+		lblMinY.setGeometry(QtCore.QRect(220, 290, 40, 20))		
 		lblMinY.setObjectName("lblMinY")
-		lbl_bbox = QtGui.QLabel("bbox:", self)
-		lbl_bbox.setGeometry(QtCore.QRect(10, 320, 40, 21))
-	
-		lbl_bbox.setObjectName("lbl_bbox")
-		lbl_TypeNames = QtGui.QLabel( "TypeNames:", self)
-		lbl_TypeNames.setGeometry(QtCore.QRect(0, 130, 80, 21))
 		
+		lbl_bbox = QtGui.QLabel("bbox:", self)
+		lbl_bbox.setGeometry(QtCore.QRect(10, 320, 40, 21))	
+		lbl_bbox.setObjectName("lbl_bbox")
+		
+		lbl_TypeNames = QtGui.QLabel( "TypeNames:", self)
+		lbl_TypeNames.setGeometry(QtCore.QRect(0, 130, 80, 21))		
 		lbl_TypeNames.setObjectName("lbl_TypeNames")
+		
 		lbl_ESPG = QtGui.QLabel("ESPG Code:", self)
 		lbl_ESPG.setGeometry(QtCore.QRect(0, 180, 81, 21))
-
 		lbl_ESPG.setObjectName("lbl_ESPG")
-		lbl_MaxFeatures = QtGui.QLabel("MaxFeatures:", self)
-		lbl_MaxFeatures.setGeometry(QtCore.QRect(0, 230, 90, 21))
 		
+		lbl_MaxFeatures = QtGui.QLabel("MaxFeatures:", self)
+		lbl_MaxFeatures.setGeometry(QtCore.QRect(0, 230, 90, 21))		
 		lbl_MaxFeatures.setObjectName("lbl_MaxFeatures")
+		
 		lbl_Operations = QtGui.QLabel("Avail Operations:", self)
-		lbl_Operations.setGeometry(QtCore.QRect(0, 80, 124, 21))
-	
+		lbl_Operations.setGeometry(QtCore.QRect(0, 80, 124, 21))	
 		lbl_Operations.setObjectName("lbl_Operations")
+		
 		cmb_Operations = QtGui.QComboBox(self)
-		cmb_Operations.setGeometry(QtCore.QRect(130, 80, 160, 21))
+		cmb_Operations.setGeometry(QtCore.QRect(130, 80, 160, 21))		
 		cmb_Operations.setObjectName("cmb_Operations")
+		
 		cmb_TypeNames = QtGui.QComboBox(self)
 		cmb_TypeNames.setGeometry(QtCore.QRect(130, 130, 160, 21))
 		cmb_TypeNames.setObjectName("cmb_TypeNames")
+		
 		lineEdit_ESPG = QtGui.QLineEdit("", self)
 		lineEdit_ESPG.setGeometry(QtCore.QRect(130, 180, 160, 21))
 		lineEdit_ESPG.setObjectName("lineEdit_ESPG")
+		
 		lineEdit_MaxFeatures = QtGui.QLineEdit("", self)
 		lineEdit_MaxFeatures.setGeometry(QtCore.QRect(130, 230, 160, 21))
 		lineEdit_MaxFeatures.setObjectName("lineEdit_MaxFeatures")
+		
 		lineEdit_MinX = QtGui.QLineEdit("", self)
 		lineEdit_MinX.setGeometry(QtCore.QRect(130, 290, 80, 21))
 		lineEdit_MinX.setObjectName("lineEdit_MinX")
+		
 		lbl_MinX = QtGui.QLabel("Min X:", self)
-		lbl_MinX.setGeometry(QtCore.QRect(80, 290, 40, 21))
-		lbl_MinX.setWordWrap(False)
+		lbl_MinX.setGeometry(QtCore.QRect(80, 290, 40, 21))		
 		lbl_MinX.setObjectName("lbl_MinX")
+		
 		lineEdit_MaxX = QtGui.QLineEdit("", self)
 		lineEdit_MaxX.setGeometry(QtCore.QRect(130, 350, 80, 21))
 		lineEdit_MaxX.setObjectName("lineEdit_MaxX")
-		lbl_MaxX = QtGui.QLabel("Max X:", self)
-		lbl_MaxX.setGeometry(QtCore.QRect(80, 350, 44, 21))
-		lbl_MaxX.setWordWrap(False)
+		
+		lbl_MaxX = QtGui.QLabel("Max X:", self)		
+		lbl_MaxX.setGeometry(QtCore.QRect(80, 350, 44, 21))	
 		lbl_MaxX.setObjectName("lbl_MaxX")
+		
 		wfs_treeView = QtGui.QTreeView(self)
 		wfs_treeView.setGeometry(QtCore.QRect(355, 81, 460, 390))
 		wfs_treeView.setWordWrap(True)
 		wfs_treeView.setObjectName("wfs_treeView")
 		
+		label_Inspect_browser = QtGui.QLabel("Inspect TypeName Metadata", self)
+		label_Inspect_browser.setGeometry(QtCore.QRect(531, 60, 201, 20))
+		label_Inspect_browser.setObjectName("label_Inspect_browser")
 		
+		loadButton = QtGui.QPushButton(self)
+		loadButton.setGeometry(QtCore.QRect(80, 470, 71, 27))
+		loadButton.setObjectName("loadButton")
+		
+		resetButton = QtGui.QPushButton(self)
+		resetButton.setGeometry(QtCore.QRect(180, 470, 71, 27))
+		resetButton.setObjectName("resetButton")
+		
+		saveButton = QtGui.QPushButton(self)
+		saveButton.setGeometry(QtCore.QRect(280, 470, 71, 27))
+		saveButton.setObjectName("saveButton")
 
+		loadButton.setText(QtGui.QApplication.translate("QTConfigurationWidget", "Load", None, QtGui.QApplication.UnicodeUTF8))
+		saveButton.setText(QtGui.QApplication.translate("QTConfigurationWidget", "Save", None, QtGui.QApplication.UnicodeUTF8))
+		resetButton.setText(QtGui.QApplication.translate("QTConfigurationWidget", "Reset", None, QtGui.QApplication.UnicodeUTF8))
 
 class WFSConfigurationWidget(OgcConfigurationWidget):
     
