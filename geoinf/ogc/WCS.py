514334c6675adb69532ca7d20262821e672b496b
Bolelang.S <Bolelang.S@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2010-11-30 14:47:18 +0000

    WCS -> WCS tab widget added,edits in progress.
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@122 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

--- geoinf/ogc/WCS.py
+++ geoinf/ogc/WCS.py
@@ -27,6 +27,7 @@
 """
 
 #from owslib.wfs import WFS
+from PyQt4 import QtCore, QtGui
 from packages.eo4vistrails.geoinf.datamodels.Raster import RasterModel
 from OgcConfigurationWidget import OgcConfigurationWidget
 from core.modules.vistrails_module import Module, new_module, NotCacheable, ModuleError
@@ -40,7 +41,117 @@ class WCS(NotCacheable,  RasterModel):
         pass
 
 
+
+class WCSCommonWidget(QtGui.QWidget):
+    """TO DO - add docstring
+
+    """
+    def __init__(self, ogc_widget, parent=None):
+        """sets parameters for wfs request"""
+        QtGui.QWidget.__init__(self, parent)
+        self.setObjectName("WCSCommonWidget")
+        self.parent_widget = ogc_widget
+        
+        self.contents = None #  only set in self.loadRequests()
+        self.create_wcs_config_window()
+        
+        # listen for signals emitted by OgcCommonWidget class
+        self.connect(
+            self.parent_widget,
+            QtCore.SIGNAL('serviceActivated'),
+            self.loadRequests
+        )
+        self.connect(
+            self.parent_widget,
+            QtCore.SIGNAL('serviceDeactivated'),
+            self.removeRequests
+        )
+        
+    def create_wcs_config_window(self):
+        """TO DO - add docstring"""
+        
+        # add widgets here!
+        self.mainLayout = QtGui.QHBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.requestsGroupBox = QtGui.QGroupBox("WCS Request Offerings")
+        self.requestsLayout = QtGui.QVBoxLayout()
+        self.requestsGroupBox.setLayout(self.requestsLayout)
+        self.mainLayout.addWidget(self.requestsGroupBox)
+        # add a horizontal split to split the window equally 
+        self.split = QtGui.QSplitter(QtCore.Qt.Vertical)
+        self.mainLayout.addWidget(self.split)
+        self.detailsGroupBox = QtGui.QGroupBox("WCS Request Details")
+        self.mainLayout.addWidget(self.detailsGroupBox)
+        self.detailsLayout = QtGui.QGridLayout()
+        self.detailsGroupBox.setLayout(self.detailsLayout)
+        # WCS Request offerings layout
+        self.requestLbx = QtGui.QListWidget()
+        self.requestsLayout.addWidget(self.requestLbx)
+        
+        # WCS Request details layout
+        # Labels
+        self.detailsLayout.addWidget(QtGui.QLabel('Layer ID:'), 0, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('Layer Geometry:'), 1, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('BBox - data bounds:'), 2, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('ULX:'), 3, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('LRX:'), 3, 2)
+        self.detailsLayout.addWidget(QtGui.QLabel('ULY:'), 4, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('LRY:'), 4, 2)
+        self.detailsLayout.addWidget(QtGui.QLabel('SRS:'), 5, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('Required Output SRS:'), 6, 0)
+        self.detailsLayout.addWidget(QtGui.QLabel('Required Output Format:'), 7, 0)
+        # Data containers
+        self.dcLayers = QtGui.QComboBox()
+        self.detailsLayout.addWidget(self.dcLayers, 0, 1)
+        self.dcLayerGeometry = QtGui.QLineEdit(' ')
+        self.detailsLayout.addWidget(self.dcLayerGeometry, 1,1)
+        self.dcULX = QtGui.QLineEdit(' ')
+        self.detailsLayout.addWidget(self.dcULX, 3,1)
+        self.dcLRX = QtGui.QLineEdit(' ')
+        self.detailsLayout.addWidget(self.dcLRX, 3,3)
+        self.dcULY = QtGui.QLineEdit(' ')
+        self.detailsLayout.addWidget(self.dcULY, 4,1)
+        self.dcLRY = QtGui.QLineEdit(' ')
+        self.detailsLayout.addWidget(self.dcLRY, 4,3)
+        self.dcSRS = QtGui.QComboBox()
+        self.detailsLayout.addWidget(self.dcSRS, 5, 1)
+        self.dcSRSreq = QtGui.QComboBox()
+        self.detailsLayout.addWidget(self.dcSRSreq, 6, 1)
+        self.dcReqFormat = QtGui.QComboBox()
+        self.detailsLayout.addWidget(self.dcReqFormat, 7, 1)
+        
+    def loadRequests(self):
+        pass
+        
+    def removeRequests(self):
+        pass
+
+
 class WCSConfigurationWidget(OgcConfigurationWidget):
     
     def __init__(self,  module, controller, parent=None):
         OgcConfigurationWidget.__init__(self,  module, controller, parent)
+        # pass in parent widget i.e. OgcCommonWidget class
+        self.wcs_config_widget = WCSCommonWidget(self.ogc_common_widget)
+        # tabs
+        self.tabs.insertTab(1, self.wcs_config_widget, "")
+        self.tabs.setTabText(
+            self.tabs.indexOf(self.wcs_config_widget),
+            QtGui.QApplication.translate(
+                "OgcConfigurationWidget",
+                "WCS Specific Metadata",
+                None,
+                QtGui.QApplication.UnicodeUTF8
+            )
+        )
+        
+        self.tabs.setTabToolTip(
+            self.tabs.indexOf(self.wcs_config_widget),
+            QtGui.QApplication.translate(
+                "OgcConfigurationWidget",
+                "Select WCS-specific parameters",
+                None,
+                QtGui.QApplication.UnicodeUTF8
+            )
+        )
+        self.tabs.setCurrentIndex(0)
