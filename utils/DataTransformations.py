3c20d137fb99ec3622cb681cd6c01ca0d24519c7
dswanepoel@gmail.com <dswanepoel@gmail.com@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2011-11-01 08:11:38 +0000

    * Correct check for missing datalist * Correct pgSQLMergeInsert's input_streams type * Improve SQL generation * Fix quoting of DEFAULT in generated SQL * Print exception for debug purposes * Disable RPyC support for now
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@547 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

--- utils/DataTransformations.py
+++ utils/DataTransformations.py
@@ -4,41 +4,43 @@ from core.modules.vistrails_module import Module, ModuleError
 from packages.eo4vistrails.rpyc.RPyC import RPyCSafeModule
 from packages.eo4vistrails.utils.ThreadSafe import ThreadSafeMixin
 
+import psycopg2
+from psycopg2 import ProgrammingError
 
-@RPyCSafeModule()
-class InputStream(ThreadSafeMixin, Module):
+#@RPyCSafeModule()
+class InputStream(Module):
     ''''''
     _input_ports = [('in_datalist', '(edu.utah.sci.vistrails.basic:List)'),
                     ('in_python_datatype', '(edu.utah.sci.vistrails.basic:String)')]
     _output_ports = [('datalist', '(edu.utah.sci.vistrails.basic:List)'),
-                    ('python_datatype', '(edu.utah.sci.vistrails.basic:String)')]                
+                    ('python_datatype', '(edu.utah.sci.vistrails.basic:String)')]
     def __init__(self):
-        ThreadSafeMixin.__init__(self)
+        #ThreadSafeMixin.__init__(self)
         Module.__init__(self)
-    
+
     def compute(self):
         d = self.getInputFromPort('in_datalist')
         t = self.getInputFromPort('in_python_datatype')
-        if d:
+        if not d is None and len(d):
             self.setResult('datalist', d)
         if t:
             self.setResult('python_datatype', t)
-            
-            
-@RPyCSafeModule()            
-class pgSQLMergeInsert(ThreadSafeMixin, Module):
+
+
+#@RPyCSafeModule()
+class pgSQLMergeInsert(Module):
     '''A Module for taking a set of lists of data and zipping them into a set of records that can be inserted into a SQL database'''
-    _input_ports = [('input_streams', '(za.co.csir.eo4vistrails:InputStream:utils)'),  \
+    _input_ports = [('input_streams', '(edu.utah.sci.vistrails.basic:List)'),  \
                             ('dbsession', '(za.co.csir.eo4vistrails:PostGisSession:postGIS)'),  \
                             ('table', '(edu.utah.sci.vistrails.basic:String)')]
-    _output_ports = [('result', '(edu.utah.sci.vistrails.basic:String)')]                
-    
-    
+    _output_ports = [('result', '(edu.utah.sci.vistrails.basic:String)')]
+
+
     def __init__(self):
-        ThreadSafeMixin.__init__(self)
+        #ThreadSafeMixin.__init__(self)
         Module.__init__(self)
         self.tblmeta = []
-    
+
     def compute(self):
         input_streams = self.getInputListFromPort('input_streams')
         db_session = self.getInputFromPort("dbsession")
@@ -50,45 +52,45 @@ class pgSQLMergeInsert(ThreadSafeMixin, Module):
                 curs.execute("select column_name, data_type, column_default from \
                 information_schema.columns where table_name = '%s';" % tbl)
                 self.tblmeta = curs.fetchall()
-                
-            colnames = ""  
+
+            colnames = ""
             default_vals = []
             for metaitem in self.tblmeta:
                 colnames = colnames + metaitem[0] + ","
-                if metaitem[2] != "":
+                if not metaitem[2] is None:
                     default_vals.append(self.tblmeta.index(metaitem))
-                
+
             colnames = colnames[:len(colnames) -1]
-            
-            
+
+
             ziprecords = zip(*input_streams)
-            
+
             insert_stmt  = "INSERT INTO %s (%s) VALUES " % (tbl,  colnames)
+            records = []
             if default_vals != []:
-               
+
                 for record in ziprecords:
                     record_as_list = list(record)
                     for dv in default_vals:
                         record_as_list.insert(dv, 'DEFAULT')
-                    
-                        insert_stmt = insert_stmt + str(tuple(record_as_list)) + ", "
+                        records.append(str(tuple(record_as_list)))
             else:
-                
+
                 for record in ziprecords:
-                    insert_stmt = insert_stmt + str(record) + ", "
-            
-            insert_stmt = insert_stmt[:len(insert_stmt)-1] + ";"
-            
+                    records.append(str(record))
+
+            insert_stmt += ", ".join(records) + ";"
+            insert_stmt = insert_stmt.replace("'DEFAULT'", "DEFAULT")
+            #print insert_stmt
+
             curs.execute(insert_stmt)
             pgconn.commit()
             self.setResult('result', curs.statusmessage)
-            
+
         except Exception as ex:
-            #print "PostGIS:292 ",ex
-            raise ModuleError, (SQLMergeInsert, \
+            print "PostGIS:292 ",ex
+            raise ModuleError, (pgSQLMergeInsert, \
                                  "Could not execute SQL Statement")
         finally:
             curs.close()
             pgconn.close()
-            
-            
