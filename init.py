b9a00c38dedc418c62150d233c01177ddfc3ca04
gamesbook@gmail.com <gamesbook@gmail.com@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2012-06-26 11:43:07 +0000

    MIGRATE: updated package init
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@870 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

--- init.py
+++ init.py
@@ -5,11 +5,17 @@ try:
 except:
     import rpyc.init as rpyc_init
 
+#brings in geo constants
+try:
+    from geoinf.geostrings import init as geostrings_init
+except:
+    import geoinf.geostrings.init as geostrings_init
+
 #brings in threading and session modules
 try:
-    from utils import init as utils_init
+    from tools.utils import init as utils_init
 except:
-    import utils.init as utils_init
+    import tools.utils.init as utils_init
 
 #brings in PostGIS modules
 try:
@@ -17,17 +23,17 @@ try:
 except:
     import geoinf.postgis.init as postgis_init
 
-#brings in ogc modules
-try:
-    from geoinf.ogc import init as ogc_init
-except:
-    import geoinf.ogc.init as ogc_init
-
 #brings in datamodels modules
 try:
     from geoinf.datamodels import init as datamodels_init
 except:
     import geoinf.datamodels.init as datamodels_init
+
+#brings in ogc and other web-based modules
+try:
+    from geoinf.web import init as web_init
+except:
+    import geoinf.web.init as web_init
 
 #brings in dataanalytics modules
 try:
@@ -35,23 +41,11 @@ try:
 except:
     import dataanalytics.init as dataanalytics_init
 
-# brings in visual widgets
-try:
-    from geoinf.visual import init as visual_init
-except:
-    import geoinf.visual.init as visual_init
-
 # brings in transformers
 try:
-    from transform import init as transform_init
+    from tools.transform import init as transform_init
 except:
-    import transform.init as transform_init
-
-# brings in datacube
-try:
-    from datacube import init as datacube_init
-except:
-    import datacube.init as datacube_init
+    import tools.transform.init as transform_init
 
 # brings in helpers
 try:
@@ -59,17 +53,35 @@ try:
 except:
     import geoinf.helpers.init as helpers_init
 
-# brings in tools
+# brings in spatial
 try:
-    from geoinf.tools import init as tools_init
+    from tools.spatial import init as spatial_init
 except:
-    import geoinf.tools.init as tools_init
+    import tools.spatial.init as spatial_init
+
+# brings in file
+try:
+    from tools.file import init as file_init
+except:
+    import tools.file.init as file_init
+
+# brings in csv
+try:
+    from tools.csv import init as csv_init
+except:
+    import tools.csv.init as csv_init
 
 # brings in plots
 try:
-    from lib.plots.standard import init as plots_init
+    from tools.visual.plots.standard import init as plots_init
 except:
-    import  lib.plots.standard.init as plots_init
+    import tools.visual.plots.standard.init as plots_init
+
+# brings in maps
+try:
+    from tools.visual.maps import init as maps_init
+except:
+    import  tools.visual.maps.init as maps_init
 
 
 def initialize(*args, **keywords):
@@ -84,14 +96,17 @@ def initialize(*args, **keywords):
 
     # NOTE! order does count
     rpyc_init.initialize(*args, **keywords)
+    geostrings_init.initialize(*args, **keywords)
     utils_init.initialize(*args, **keywords)
     datamodels_init.initialize(*args, **keywords)
-    ogc_init.initialize(*args, **keywords)
-    postgis_init.initialize(*args, **keywords)
-    visual_init.initialize(*args, **keywords)
-    dataanalytics_init.initialize(*args, **keywords)
+    web_init.initialize(*args, **keywords)
+    #dataanalytics_init.initialize(*args, **keywords)
     transform_init.initialize(*args, **keywords)
-    datacube_init.initialize(*args, **keywords)
     helpers_init.initialize(*args, **keywords)
-    tools_init.initialize(*args, **keywords)
+    spatial_init.initialize(*args, **keywords)
     plots_init.initialize(*args, **keywords)
+    maps_init.initialize(*args, **keywords)
+    csv_init.initialize(*args, **keywords)
+    file_init.initialize(*args, **keywords)
+    postgis_init.initialize(*args, **keywords)
+    dataanalytics_init.initialize(*args, **keywords)
