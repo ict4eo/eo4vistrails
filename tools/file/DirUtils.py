2c1455736769d0d73affc0b0edfb95a08e9d7b2c
gamesbook@gmail.com <gamesbook@gmail.com@5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec>
2012-06-26 08:32:20 +0000

    MIGRATE: added files to tools/file
    git-svn-id: https://eo4vistrails.googlecode.com/svn/trunk@860 5a402d0d-53e9-19e5-31b6-78a5cb6ef9ec

---
+++ tools/file/DirUtils.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+############################################################################
+###
+### Copyright (C) 2010 CSIR Meraka Institute. All rights reserved.
+###
+### This full package extends VisTrails, providing GIS/Earth Observation
+### ingestion, pre-processing, transformation, analytic and visualisation
+### capabilities . Included is the abilty to run code transparently in
+### OpenNebula cloud environments. There are various software
+### dependencies, but all are FOSS.
+###
+### This file may be used under the terms of the GNU General Public
+### License version 2.0 as published by the Free Software Foundation
+### and appearing in the file LICENSE.GPL included in the packaging of
+### this file.  Please review the following to ensure GNU General Public
+### Licensing requirements will be met:
+### http://www.opensource.org/licenses/gpl-license.php
+###
+### If you are unsure which license is appropriate for your use (for
+### instance, you are interested in developing a commercial derivative
+### of VisTrails), please contact us at vistrails@sci.utah.edu.
+###
+### This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+### WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+###
+#############################################################################
+"""This module forms part of the eo4vistrails capabilities. It is used to
+handle directory related queries.
+"""
+# library
+import os
+import os.path
+# vistrails
+from core.modules.vistrails_module import Module, ModuleError
+# eo4vistrails
+from packages.eo4vistrails.rpyc.RPyC import RPyCSafeModule
+from packages.eo4vistrails.tools.utils.ThreadSafe import ThreadSafeMixin
+
+
+@RPyCSafeModule()
+class ListDirContent(ThreadSafeMixin, Module):
+    """A utility for walking a directory to discover files with specified
+    extensions.
+
+    Input ports:
+        directory path:
+            place in which files are to be discovered
+        file_extensions:
+            a list of types of files to be discovered
+
+    Output ports:
+        csv_list:
+            A list of full filenames. On RPyC nodes, will refer to files on
+            that remote filesystem.
+
+    """
+
+    _input_ports = [('directorypath', '(edu.utah.sci.vistrails.basic:String)'),
+                    ('file_extensions', '(edu.utah.sci.vistrails.basic:List)')]
+    _output_ports = [('file_list', '(edu.utah.sci.vistrails.basic:List)')]
+
+    def __init__(self):
+        ThreadSafeMixin.__init__(self)
+        Module.__init__(self)
+
+    def compute(self):
+        filename_list = []
+        dp = self.getInputFromPort('directorypath')
+        fel = self.getInputFromPort('file_extensions')
+
+        def _index(directory):
+            #stack = [directory]
+            files = []
+            #while stack:
+                #directory = stack.pop()
+            for filename in os.listdir(directory):
+                fullname = os.path.join(directory, filename)
+                files.append(fullname)
+                    #if os.path.isdir(fullname) and not os.path.islink(fullname):
+                    #    stack.append(fullname)
+            return files
+
+        for fname in _index(dp):
+            try:
+                if fname.split(".")[-1] in fel:
+                    filename_list.append(fname)
+            except:
+                #likely a directory, ignore
+                pass
+
+        self.setResult('file_list', filename_list)
---
+++ tools/file/init.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+############################################################################
+###
+### Copyright (C) 2010 CSIR Meraka Institute. All rights reserved.
+###
+### This full package extends VisTrails, providing GIS/Earth Observation
+### ingestion, pre-processing, transformation, analytic and visualisation
+### capabilities . Included is the abilty to run code transparently in
+### OpenNebula cloud environments. There are various software
+### dependencies, but all are FOSS.
+###
+### This file may be used under the terms of the GNU General Public
+### License version 2.0 as published by the Free Software Foundation
+### and appearing in the file LICENSE.GPL included in the packaging of
+### this file.  Please review the following to ensure GNU General Public
+### Licensing requirements will be met:
+### http://www.opensource.org/licenses/gpl-license.php
+###
+### If you are unsure which license is appropriate for your use (for
+### instance, you are interested in developing a commercial derivative
+### of VisTrails), please contact us at vistrails@sci.utah.edu.
+###
+### This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+### WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+###
+#############################################################################
+"""This module is called by higher level inits to ensure that registration
+with VisTrails takes place
+"""
+
+from core.modules.module_registry import get_module_registry
+
+def initialize(*args, **keywords):
+    from core.modules import basic_modules
+    from core.modules.vistrails_module import Module
+
+    from Command import Command
+    from DirUtils import ListDirContent
+    from StringToFile import StringToFile
+    from DataWriter import VectorLayerToFile, DataWriterTypeComboBox
+
+    reg = get_module_registry()
+    files_namespace = "tools|file"
+
+    # =========================================================================
+    # Abstract Modules - these MUST appear FIRST
+    # =========================================================================
+
+    # drop-down lists
+    reg.add_module(DataWriterTypeComboBox,
+                   namespace=files_namespace,
+                   abstract=True)
+
+    # =========================================================================
+    # ComboBox definitions
+    # =========================================================================
+
+    # =========================================================================
+    # Standard Modules - Ports defined here
+    # =========================================================================
+
+    # =========================================================================
+    # Control Flow Modules -
+    # =========================================================================
+
+    # =========================================================================
+    # Other Modules - without ports OR with locally defined ports
+    # =========================================================================
+
+
+    reg.add_module(ListDirContent,
+                   namespace=files_namespace)
+
+    reg.add_module(StringToFile,
+                   namespace=files_namespace)
+
+    reg.add_module(VectorLayerToFile,
+                   namespace=files_namespace)
